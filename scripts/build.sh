041d46cd9fc4743707d8a2581ed117098b4dd614
Petr Tripolsky <tripolskypetr@gmail.com>
2022-03-04 18:26:41 +0300

    build-script

---
+++ scripts/build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+cd types
+targets=( @emotion @popperjs @mui )
+for D in "${targets[@]}"
+do
+    cd $D
+    echo Working on ${D}
+    node ../../node_modules/typescript/bin/tsc
+    cp ./_dist/${D}.d.ts ../../dist
+    cd ..
+done
